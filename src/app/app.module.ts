07e01325e9d9a623220bec40fb4a8dc54c0cd133
Michael Sorens <birdwatcher42@gmail.com>
2021-07-13 09:30:13 -0700

    Tweaks to get to a working state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgxsModule } from '@ngxs/store';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { VirtualScrollerModule } from 'ngx-virtual-scroller';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountState } from './app.state';
-import { ViewPostsComponent } from './posts/view-posts/view-posts.component';
+import { PostsModule } from './posts/posts.module';
 import { PostState } from './state/post.store';
 
 @NgModule({
-  declarations: [AppComponent, ViewPostsComponent],
+  declarations: [AppComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
     MatProgressSpinnerModule,
-    NgxsModule.forRoot([CountState, PostState ]),
-    VirtualScrollerModule,
-    NoopAnimationsModule
+    NgxsModule.forRoot([CountState, PostState]),
+    PostsModule,
+    NoopAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -2,12 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PostsRouting } from './post.routing';
 import { ViewPostsComponent } from './view-posts/view-posts.component';
+import { VirtualScrollerModule } from 'ngx-virtual-scroller';
 
 @NgModule({
   imports: [
     CommonModule,
-    PostsRouting
+    PostsRouting,
+    VirtualScrollerModule
   ],
-  declarations: [ViewPostsComponent]
+  declarations: [ViewPostsComponent],
+  exports: [
+    ViewPostsComponent
+  ],
 })
 export class PostsModule { }
