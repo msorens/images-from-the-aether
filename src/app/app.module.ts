1ece7b1d4bd28a3a9ba12d582e0f84498481f08d
Michael Sorens <birdwatcher42@gmail.com>
2021-07-16 14:38:55 -0700

    Instrument redux dev tools
    Package:
    npm install @ngxs/devtools-plugin --save-dev
    
    Configuration:
    https://www.ngxs.io/plugins/devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,11 +20,15 @@ import { PostState } from './state/post.store';
     HttpClientModule,
     MatProgressSpinnerModule,
     NgxsModule.forRoot([PostState]),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: !isDevMode(), // disable for production builds
+      maxAge: 25, // max number of entries
+    }),
     PostsModule,
     ReactiveFormsModule,
     NoopAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
