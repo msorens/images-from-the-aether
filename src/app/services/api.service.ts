09a83e164725a38b921b21bcc07e68c409177b8f
Michael Sorens <birdwatcher42@gmail.com>
2021-07-18 20:27:54 -0700

    Add retry for API calls

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 import { Store } from '@ngxs/store';
 import { PageResponse } from '../models/Post';
+import { delayWhen, retryWhen, shareReplay, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -15,14 +16,24 @@ export class ApiService {
     const url = 'https://api.pexels.com/v1/search';
     const apiKey = 'FILL THIS IN';
     return this.http.get<PageResponse>(url, {
-      headers: {
-        Authorization: apiKey
-      },
-      params: {
-        query: searchString,
-        per_page: String(itemsPerPage),
-        page: String(pageId)
-      }
-    });
+        headers: {
+          Authorization: apiKey
+        },
+        params: {
+          query: searchString,
+          per_page: String(itemsPerPage),
+          page: String(pageId)
+        }
+      })
+      // cf https://blog.angular-university.io/rxjs-error-handling/
+      .pipe(
+        shareReplay(),
+        retryWhen((errors) => {
+          return errors.pipe(
+            delayWhen(() => timer(2000)),
+            tap(() => console.log('retrying...'))
+          );
+        })
+      );
   }
 }
