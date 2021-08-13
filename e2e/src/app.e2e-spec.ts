8939b89635b24ec7524ef8b2dde328bc7d0cf797
Michael Sorens <birdwatcher42@gmail.com>
2021-08-13 15:21:41 -0700

    Rename app

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('photo-gallery app is running!');
+    expect(await page.getTitleText()).toEqual('images-from-the-aether app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/photo-gallery'),
+      dir: require('path').join(__dirname, './coverage/images-from-the-aether'),
       subdir: '.',
       reporters: [
         { type: 'html' },
